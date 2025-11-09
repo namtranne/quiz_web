ce2d24819941866176f94d9b56ed83ad9d9e90ac
Trần Hải Nam <steven.hai.ta856@gmail.com>
2025-11-10 01:11:21 +0700

    Init

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: false,
+	images: {
+		domains: ['doidepfmcg.com'] // add all external domains you want
+	}
+}
+
+module.exports = nextConfig
